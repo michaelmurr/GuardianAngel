23bffefee3d27cc6ca5f5b91550e9f328cb731c1
Xentox-Phil <philkristen@t-online.de>
2025-05-23 17:07:27 +0000

    ✨ updated tracking_task

--- backend/test_tracking.py
+++ backend/test_tracking.py
@@ -1,13 +1,21 @@
 import time
 
 import valkey
+from app.pubsub.tracking_task import get_tracking_tasks_key
 from app.repositories.valkey import get_valkey
+from app.types.tracking import TrackingTaskMessage
 
 _vk_client: valkey.client.Valkey = get_valkey()
 
+
 _vk_client.publish(
-    "tracking_tasks",
-    '{"uid": "user_2xTGnCK2HjOOvj41oUiSxinRXcW","device_id": "YOUR_DEVICE_ID", "action": "START", "destination": {"longitude": 123.5, "latitude": 70.3}}',
+    get_tracking_tasks_key(),
+    TrackingTaskMessage(
+        uid="user_2xTGnCK2HjOOvj41oUiSxinRXcW",
+        device_id="YOUR_DEVICE_ID",
+        action="START",
+        polyline="cgcjHifzhAWC?S?_@?kACoFH@@EBg@QEPcA\\_BNgA?a@LcCDg@Jy@LwD?MUAWA?He@CcAEE]D}HA[?m@XcFiBo@",
+    ).model_dump_json(),
 )
 # time.sleep(1)
 # _vk_client.publish(
@@ -50,7 +58,6 @@ _vk_client.publish(
 #     "123",
 #     '{"battery": 100, "speed": 20.3, "location": {"longitude": 12.097622408462362, "latitude": 49.0040969323266}}',
 # )
-
 
 
 time.sleep(60)
